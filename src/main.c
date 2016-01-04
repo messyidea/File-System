cae3bb205d40ef0d150ebbd2bbb500a93643d599
messyidea <the.mid.sea@gmail.com>
2016-01-04 21:36:43 +0800

    add signal

--- src/main.c
+++ src/main.c
@@ -4,6 +4,9 @@
 #include "init.h"
 #include "login.h"
 #include <time.h>
+#include <signal.h>
+
+
 
 /*
 super_node 1024字节
@@ -12,6 +15,15 @@ block 512字节
 1 block = 8 inode
 (2 block) (1 block) (512 inode = 64 block) (1024 block)
 */
+
+void call_for_exit() {
+    puts("");
+    puts("wait for save data");
+    save();
+    puts("save ok");
+    exit(0);
+}
+
 int main()
 {
     /*
@@ -27,6 +39,11 @@ int main()
     printf("size char = %d\n", sizeof(char));
     printf("sizeof ctime = %d\n", sizeof(time_t));
     */
+
+    signal(SIGINT,call_for_exit);
+    signal(SIGQUIT,call_for_exit);
+	signal(SIGKILL,call_for_exit);
+	signal(SIGTERM,call_for_exit);
 
     init();
     //debug_show_dir(0);
